a919a3e8d2938409c743cee336cd52e08ad25b99
DOSSEH Shalom <114688102+AnalyticAce@users.noreply.github.com>
2024-06-08 03:10:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,21 +72,18 @@ def handle_generate_page(app: AppSection, API_KEY: str) -> None:
     generation = st.button('***:blue[Generate Yo]:red[ur Resume]***', help='Hover over me!')
 
     if generation and template_1:
-        try:
-            path = subprocess.check_output(['which', 'tesseract'])
-            print(path)
-        except subprocess.CalledProcessError:
-            print("Tesseract not found in PATH")
-
-        # if 'temp_file' not in locals() or not temp_file:
-        #     st.warning("Please upload a file before generating your resume.", icon='📑')
-        #     st.stop()
+        st.write('We are are currently working on deployement, if you want to test the project visit https://github.com/AnalyticAce/MyResumo and clone of the main branch')
+        """
+        if 'temp_file' not in locals() or not temp_file:
+            st.warning("Please upload a file before generating your resume.", icon='📑')
+            st.stop()
         
-        # if not job_description:
-        #     st.warning("Please enter the job description before generating your resume.", icon='📑')
-        #     st.stop()
+        if not job_description:
+            st.warning("Please enter the job description before generating your resume.", icon='📑')
+            st.stop()
         
-        # generate_resume(temp_file, images, job_description, resume_tone, language, API_KEY, user_name, vision, tool, color_code)
+        generate_resume(temp_file, images, job_description, resume_tone, language, API_KEY, user_name, vision, tool, color_code)
+        """
 
 def generate_resume(temp_file: str, images: str, job_description: str,
         resume_tone: str, language: str, API_KEY: str, user_name: str, 
