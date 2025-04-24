a5cd2d103cbd2a699caf96be00e02042b136d9b9
AnalyticAce <shalom.dosseh@epitech.eu>
2025-04-24 11:16:12 +0100

    ci(publish-release) update release workflow with python-semantic-release

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,5 @@
 This package contains all the modules and subpackages that make up the MyResumo application,
 a resume creation and optimization system using AI technologies.
 """
+
+__version__ = '1.0.0'
